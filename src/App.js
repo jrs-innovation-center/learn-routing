87f5bb63a120d53d2b3bbadad011e8a5fb676782
tripott <tripott@gmail.com>
2018-07-12 08:49:58 -0400

    set up buzzwords recreate demo.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,6 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Colors from './pages/colors'
-import ShowColor from './pages/colors/show'
-import StarWars from './pages/starwars'
-import ShowStarWarsName from './pages/starwars/show'
-import BuzzWords from './pages/buzzwords'
-import ShowBuzzWord from './pages/buzzwords/show'
-import Cookies from './pages/cookies'
-import ShowCookie from './pages/cookies/show'
-import TestWithRouterPage from './pages/test-withrouter-page';
-import Menu from './pages/menu'
 
-const App = props => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Menu} />
-          <Route exact path="/colors" component={Colors} />
-          <Route exact path="/colors/:id" component={ShowColor} />
-          <Route exact path="/starwars" component={StarWars} />
-          <Route exact path="/starwars/:id" component={ShowStarWarsName} />
-          <Route exact path="/buzzwords" component={BuzzWords} />
-          <Route exact path="/buzzwords/:id" component={ShowBuzzWord} />
-          <Route exact path="/cookies" component={Cookies} />
-          <Route exact path="/cookies/:id" component={ShowCookie} />
-          <Route exact path="/testwithrouterpage" component={TestWithRouterPage} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  )
-}
+const App = props => <h1>Welcome to Routing</h1>
 
 export default App
 
--- src/pages/test-withrouter-page/index.js
+++ src/pages/test-withrouter-page/index.js
@@ -1,8 +1,1 @@
-import React from "react";
-import { withRouter } from "react-router";
 
-const TestWithRounterPage = ({ match, location, history }) => (
-  <div> You are now at {location.pathname} </div>
-);
-
-export default withRouter(TestWithRounterPage);
