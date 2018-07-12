456c585497e35a6ea0fbbabede37e41093c8436f
tripott <tripott@gmail.com>
2018-07-12 09:19:01 -0400

    intro text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
-const App = props => <h1>Welcome to Routing</h1>
+const App = props => {
+    
+  return  (<div>
+      <h1>Welcome to Routing</h1>
+        <p>This demo branch contains some missing components and existing components. In this demo we will build the following:</p>
+        <ul>
+            <li>Create routes for existing pages/components.</li>
+            <li>Create buzzwords list and show pages</li>
+            <li>Build a very simple component that uses the <code>withRouter()</code> Higher Order Component (HOC).</li>
+        </ul>
+</div>)
+
+}
 
 export default App
 
